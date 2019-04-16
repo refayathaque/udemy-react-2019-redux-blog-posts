f863dbd388800da4c95c8aa1ecbe7181d10f14bd
Refayat Haque <refayathaque@gmail.com>
2019-04-16 19:46:32 -0400

    staged, committed and pushing to master

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,13 +13,13 @@ export const fetchPosts = () => {
   }
 }
 
-// Async actions with Redux-Thunk
-// With Redux-Thunk we can return a function with our action creators (instead of only a plain action object), and when we do this, the function will be invoked with the 'dispatch' and 'getState' (optional) functions from Redux as arguments
-// With these ^ two Redux functions we have the ability to control the Redux store in any way we want (e.g., can STOP/MODIFY actions)
-// This allows us to wait until the API request is complete, and once the request is complete, we can DISPATCH THE ACTION MANUALLY
+// Async actions with Redux-Thunk middleware
+// With Redux-Thunk we can RETURN A FUNCTION with our action creators (instead of only a plain action object), and when we do this, the function will be invoked with the 'dispatch' and 'getState' (optional) functions from Redux as arguments
+// With these ^ two Redux functions we have the ability to control the Redux store in any way we want (e.g., can STOP/MODIFY/DELAY actions)
+// This allows us to wait until an API request is complete, and once the request is complete, we can DISPATCH THE ACTION MANUALLY
 
 // Redux-Thunk, by virtue of being a middleware, works at the layer between Redux 'dispatch' and 'reducers'
 // All actions dispatched will go through Redux-Thunk, and it will determine if the action is a function or a plain action object
 // If Redux-Thunk gets a plain action object, it will forward it to the reducers (just how Redux does things)
-// If Redux-Thunk gets a function it will run the function, this function will make the API request, and then DISPATCH THE ACTION MANUALLY once the API request is complete
+// If Redux-Thunk gets a function it will run the function, this function (in most cases) will make an API request, and then DISPATCH THE ACTION MANUALLY once the API request is complete
 // ^ This blocks the normal Redux flow of 'action -> reducer', and only resumes the flow once the API request is complete
