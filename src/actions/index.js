a3db66282a369ad03347dbf8524dc22e3281ffc9
Refayat Haque <refayathaque@gmail.com>
2019-04-16 18:19:39 -0400

    staged, committed and pushing to master

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,11 @@
 import jsonPlaceholder from 'apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-
   console.log('ACTION CREATOR, fetchPosts')
 
   return async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
-    console.log(response)
+    console.log('ACTION CREATOR, fetchPosts, response: ', response)
     dispatch({
       type: 'FETCH_POSTS',
       payload: response
@@ -15,7 +14,7 @@ export const fetchPosts = () => {
 }
 
 // Async actions with Redux-Thunk
-// With Redux-Thunk we can return a function with our action creators (instead of only a plain action object), and when we do this, the function will be invoked with the 'dispatch' and 'getState' functions from Redux as arguments
+// With Redux-Thunk we can return a function with our action creators (instead of only a plain action object), and when we do this, the function will be invoked with the 'dispatch' and 'getState' (optional) functions from Redux as arguments
 // With these ^ two Redux functions we have the ability to control the Redux store in any way we want (e.g., can STOP/MODIFY actions)
 // This allows us to wait until the API request is complete, and once the request is complete, we can DISPATCH THE ACTION MANUALLY
 
