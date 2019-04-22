6a02b369a2e0815be24727f1952715ad01ea0c2a
Refayat Haque <refayathaque@gmail.com>
2019-04-22 12:27:04 -0400

    staged, committed and pushing to master

--- src/actions/fetchPostsAndUsers.js
+++ src/actions/fetchPostsAndUsers.js
@@ -12,7 +12,7 @@ const fetchPostsAndUsers = () => {
     // Whenver we call an action creator from INSIDE of ANOTHER action creator, we need to make sure that we DISPATCH the result of calling the action creator
     const userIds = _.uniq(_.map(getState().posts, 'userId'))
     console.log(userIds)
-    // forEach() vs. map() - https://codeburst.io/javascript-map-vs-foreach-f38111822c0f
+    // forEach() vs. map() - https://codeburst.io/javascript-map-vs-foreach-f38111822c0f - forEach() method doesn’t actually return anything (undefined). It simply calls a provided function on each element in your array.
     userIds.forEach(id => dispatch(fetchUser(id)))
     // Don't need `await` keyword when dispatching `fetchUser` action creator, because we do not have anything else to do in this code block after we fetch the user data. I.e., we do not have anything that is dependent on the results of `fetchUser`, unlike with `fetchPosts` where we needed that action creator to succesfully complete in order to create our array of `userIds`
   }
